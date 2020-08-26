53edc2ff64aabcf4af5f125f724ece0c749d2905
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-26 03:27:11 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    if params[:name] != nil
+    if params[:name] != ''
       session[:name]=params[:name]
       redirect_to '/'
     else

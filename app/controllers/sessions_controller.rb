788d60f2dc3644efd90193787286b3123a4f539e
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-26 03:15:47 +0000

    Automatically backed up by Learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    session[:name]=params[:name]
+    redirect_to '/'
+  end 
+  def destroy
+    session.delete :name
+  end
+end
